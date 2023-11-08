e0de4190375113c4c6c058e0db9174458aa53227
igopib <gs109690@gmail.com>
2023-11-07 18:45:00 -0600

    feat: using shadcn/ui and other requirements

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
 import type { Metadata } from 'next'
+import { ThemeProvider } from '@/components/themeprovider'
 import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Gopinho - Profilev2',
   description: 'Personal webapp',
 }
+
+import Ocrb from 'next/font/local'
+const ocrb = Ocrb({ src: '../public/fonts/ocrb.otf' })
 
 export default function RootLayout({
   children,
@@ -13,7 +17,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className={`${ocrb.className} relative flex h-screen`}>
+            {children}
+          </div>
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
