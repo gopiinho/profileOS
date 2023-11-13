a0e05aed46d692952b5d8e8a75fe3c6745c1b4a2
igopib <gs109690@gmail.com>
2023-11-12 18:33:58 -0600

    feat: about has switching profile gifs

--- components/about.tsx
+++ components/about.tsx
@@ -1,15 +1,20 @@
+'use client'
+
 import Image from 'next/image'
 import Link from 'next/link'
 import { Card, CardHeader, CardIcon, CardTitle } from '@/components/ui/card'
 import { Button } from './ui/button'
 import { CardProps } from '@/utils/constants'
 import AnimatedHeading from './animatedText'
+import { useTheme } from 'next-themes'
 
 // Icons
 import { GiAbstract029 } from 'react-icons/gi'
-import profile from '@/public/assets/about.gif'
+import darkPic from '@/public/assets/about.gif'
+import lightPic from '@/public/assets/about2.gif'
 
 export default function About({ title }: CardProps) {
+  const { theme } = useTheme()
   return (
     <div className='flex items-center justify-center'>
       <Card className='w-[600px] lg:w-[700px]'>
@@ -35,7 +40,11 @@ export default function About({ title }: CardProps) {
               building their webapp and frontends.
             </span>
             <div className='flex h-28 w-28 shrink-0'>
-              <Image src={profile} alt='profile pic' />
+              {theme == 'light' ? (
+                <Image src={lightPic} alt='profile pic' />
+              ) : (
+                <Image src={darkPic} alt='profile pic' />
+              )}
             </div>
           </div>
           <div className='p-4'></div>
