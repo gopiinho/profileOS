5fd8e99c4bfe98f825751f8465be7f5c3ca2c7d6
igopib <gs109690@gmail.com>
2023-11-14 00:41:28 -0600

    refactor: component test on ui

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,13 @@ import Skills from '@/components/skills'
 import EthPrice from '@/components/ethPrice'
 import Contact from '@/components/contact'
 import About from '@/components/about'
+import RockPaperScissor from '@/components/rockpaperscissor'
 
 export default function Home() {
   return (
     <div className='relative flex w-full items-center justify-center'>
       <EthPrice />
-      <About title='about' />
+      <RockPaperScissor title='RoPaSc' />
     </div>
   )
 }
