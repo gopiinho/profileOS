8b3d814337ccbf95b194782e7a1c795ef9a32030
igopib <gs109690@gmail.com>
2023-12-09 00:24:36 -0600

    feat: wagmi wrapper for next app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,51 @@
-import type { Metadata } from 'next'
+'use client'
 import { ThemeProvider } from '@/components/themeProvider'
 import './globals.css'
 
-export const metadata: Metadata = {
-  title: 'Gopinho - Profilev2',
-  description: 'Personal webapp',
-}
-
 import Ocrb from 'next/font/local'
 const ocrb = Ocrb({ src: '../public/fonts/ocrb.otf' })
+
+// Wallet Integrations using Wagmi and Viem
+import { WagmiConfig, sepolia, createConfig, configureChains } from 'wagmi'
+import { publicProvider } from 'wagmi/providers/public'
+
+import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [sepolia],
+  [publicProvider()]
+)
+
+const config = createConfig({
+  autoConnect: true,
+  connectors: [
+    new MetaMaskConnector({ chains }),
+    new CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName: 'wagmi',
+      },
+    }),
+    new WalletConnectConnector({
+      chains,
+      options: {
+        projectId: '...',
+      },
+    }),
+    new InjectedConnector({
+      chains,
+      options: {
+        name: 'Injected',
+        shimDisconnect: true,
+      },
+    }),
+  ],
+  publicClient,
+  webSocketPublicClient,
+})
 
 export default function RootLayout({
   children,
@@ -16,17 +53,19 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang='en' className={ocrb.className}>
-      <body>
-        <ThemeProvider
-          attribute='class'
-          defaultTheme='dark'
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
+    <WagmiConfig config={config}>
+      <html lang='en' className={ocrb.className}>
+        <body>
+          <ThemeProvider
+            attribute='class'
+            defaultTheme='dark'
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </WagmiConfig>
   )
 }
