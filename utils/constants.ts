26afd5b9db12a898424f22bdd15c3f94ca7ce410
igopib <gs109690@gmail.com>
2023-12-09 22:47:36 -0600

    chore: contract address and abi for interaction

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,6 +1,8 @@
 import { StaticImageData } from 'next/image'
 
 export const coinGeckoApiUrl = 'https://api.coingecko.com/api/v3/'
+export const tipjarContractAddress =
+  '0x1f34d9f6b67e8df6bdcbecb8c09b598fc724785e'
 
 export interface CardProps extends React.HTMLAttributes<HTMLElement> {
   title: string
