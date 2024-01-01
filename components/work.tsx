4fe38d0a84d2d9b051e5cfba3a0d92c80af80a35
Gurpreet <gs109690@gmail.com>
2024-01-01 12:18:38 -0600

    feat: project added in work

--- components/work.tsx
+++ components/work.tsx
@@ -12,6 +12,7 @@ import { BsFileCodeFill } from 'react-icons/bs'
 import enderApp from '@/public/app.png'
 import sibyl from '@/public/sibyl.png'
 import ender from '@/public/ep.png'
+import enderv1 from '@/public/epv1.png'
 import profileOs from '@/public/profileos.png'
 
 const ProjectItems: ProjectProps[] = [
@@ -21,7 +22,14 @@ const ProjectItems: ProjectProps[] = [
       'Dapp built from scratch using next.js and typescript. Wallet & smart contract integrations handled with rainbow kit and wagmi.sh hooks.',
     url: 'https://app.enderprotocol.io',
     gurl: 'https://github.com/enderprotocol/enderprotocol-app',
-    stack: ['Next.js', 'Typescript', 'Tailwindcss', 'Wagmi.sh', 'Rainbow Kit'],
+    stack: [
+      'next.js',
+      'typescript',
+      'tailwindcss',
+      'wagmi.sh',
+      'rainbow kit',
+      'axios',
+    ],
     img: enderApp,
   },
   {
@@ -30,16 +38,25 @@ const ProjectItems: ProjectProps[] = [
       'React app, styled by chakraui and smart contract integration with etherjs. Focus being the theme of the world that nfts take place in.',
     url: 'https://whimsical-kheer-5221bd.netlify.app/',
     gurl: 'https://github.com/igopib/sibyl-access',
-    stack: ['Solidity', 'React', 'ChakraUI', 'Ethers.js'],
+    stack: ['solidity', 'react', 'chakraUI', 'ethers.js'],
     img: sibyl,
   },
   {
-    name: 'Ender Protocol',
+    name: 'Ender Protocol V1 - Landing',
+    description:
+      'Landing page for the Ender Protocol V1. Built using Astro and Shadcn-ui.',
+    url: 'https://whimsical-kheer-5221bd.netlify.app/',
+    gurl: 'https://github.com/igopib/sibyl-access',
+    stack: ['astro', 'tailwindcss', 'typescript', 'shadcn-ui'],
+    img: enderv1,
+  },
+  {
+    name: 'Ender Protocol - Landing',
     description:
       'Landing page created with layouts and custom css. Heavy use of gradients and custom styling.',
     url: 'https://enderprotocol.io',
     gurl: 'https://github.com/enderprotocol/enderprotocol-fe',
-    stack: ['Next.js', 'Tailwindcss', 'CSS'],
+    stack: ['next.js', 'tailwindcss', 'css'],
     img: ender,
   },
 ]
